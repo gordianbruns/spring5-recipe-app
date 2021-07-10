cbc9af8326f2dc506e038c7f092f812dcb8d4c30
gordianbruns <gordob1@web.de>
2021-07-10 10:36:48 -0400

    added ingredient POJO

--- src/main/java/edu/depauw/recipeapp/model/Recipe.java
+++ src/main/java/edu/depauw/recipeapp/model/Recipe.java
@@ -1,6 +1,7 @@
 package edu.depauw.recipeapp.model;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 public class Recipe {
@@ -16,6 +17,9 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
 
     @Lob
     private Byte[] image;
@@ -93,6 +97,14 @@ public class Recipe {
 
     public void setImage(Byte[] image) {
         this.image = image;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
     }
 
     public Notes getNotes() {
