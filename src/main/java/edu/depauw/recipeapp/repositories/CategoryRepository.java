9e17437813c57522cfca1714e9ee95b64bfe37b1
gordianbruns <gordob1@web.de>
2021-07-11 17:42:56 -0400

    added findByDescription query for UOM and Category

--- src/main/java/edu/depauw/recipeapp/repositories/CategoryRepository.java
+++ src/main/java/edu/depauw/recipeapp/repositories/CategoryRepository.java
@@ -3,5 +3,9 @@ package edu.depauw.recipeapp.repositories;
 import edu.depauw.recipeapp.model.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+    Optional<Category> findByDescription(String description);
 }
